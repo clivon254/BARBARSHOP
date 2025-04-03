c9dd188018a77a8e96264dcf765f130d5842a4f6
Clivon Osire <clivon254@icloud.com>
2025-04-03 12:44:05 +0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,61 @@
+
+import express from "express"
+import cors from "cors"
+
+
+
+const app = express()
+
+
+const PORT = process.env.PORT 
+
+
+app.use(cors())
+
+
+app.use(express.json())
+
+
+// DB CONNECTION
+mongoose.connect(process.env.MONGO_URL)
+.then(() => console.log("DB CONNECTED"))
+.catch((err) => console.log(err))
+
+
+
+// API
+app.get('/',(req,res) => {
+
+    res.send("HELLO BARBAR APPOINTMENT")
+
+})
+
+
+
+
+// LISTENING
+app.listen(PORT,(err) => {
+
+    if(err)
+    {
+        console.log(err.message)
+    }
+    else
+    {
+        console.log(`SERVER RUNNING ON PORT ${PORT}`)
+    }
+
+})
+
+
+
+// middleware
+app.use((err,req,res,next) => {
+
+    const statusCode = err.statusCode || 500
+
+    const message = err.message || "Internal Server Error"
+
+    res.status(statusCode).json({success:false , message:message})
+
+})
