d21a9ffbe87e52bfbb067b9ef01ab9e00142baa6
Clivon Osire <clivon254@icloud.com>
2025-04-07 20:37:13 +0300

    front-end installation

---
+++ admin/src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import React from 'react'
+
+export default function App() {
+
+  return (
+
+   <div className="flex h-screen items-center justify-center">
+
+      <h2 className="text-3xl font-semibold">Welcome to the barbarshop</h2>
+
+   </div>
+
+  )
+
+}
